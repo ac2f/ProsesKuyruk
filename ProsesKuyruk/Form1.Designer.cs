f58d2ee4ff68d74623125b9f4e42997ba4c9245b
mustafw <mustafabasarn3@gmail.com>
2021-12-26 12:46:32 +0300

    Add project files.

---
+++ ProsesKuyruk/Form1.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace ProsesKuyruk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnIslemciBaslat = new System.Windows.Forms.Button();
+            this.btnIslemciDurdur = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.lstBitirilenProsesler = new System.Windows.Forms.ListBox();
+            this.btnBirilenProsesleriGoster = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblIslemcıDurumu = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.process1 = new System.Diagnostics.Process();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Proses Kuyruğu";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(26, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.richTextBox1.Size = new System.Drawing.Size(317, 29);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btnIslemciBaslat
+            // 
+            this.btnIslemciBaslat.Location = new System.Drawing.Point(349, 36);
+            this.btnIslemciBaslat.Name = "btnIslemciBaslat";
+            this.btnIslemciBaslat.Size = new System.Drawing.Size(134, 35);
+            this.btnIslemciBaslat.TabIndex = 3;
+            this.btnIslemciBaslat.Text = "İŞLEMCİ BAŞLAT";
+            this.btnIslemciBaslat.UseVisualStyleBackColor = true;
+            this.btnIslemciBaslat.Click += new System.EventHandler(this.btnIslemciBaslat_Click);
+            // 
+            // btnIslemciDurdur
+            // 
+            this.btnIslemciDurdur.Location = new System.Drawing.Point(349, 77);
+            this.btnIslemciDurdur.Name = "btnIslemciDurdur";
+            this.btnIslemciDurdur.Size = new System.Drawing.Size(134, 35);
+            this.btnIslemciDurdur.TabIndex = 3;
+            this.btnIslemciDurdur.Text = "İŞLEMCİ DURDUR";
+            this.btnIslemciDurdur.UseVisualStyleBackColor = true;
+            this.btnIslemciDurdur.Click += new System.EventHandler(this.btnIslemciDurdur_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(577, 36);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Proses 1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(663, 36);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(67, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Proses 2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(749, 36);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(67, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Proses 3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // lstBitirilenProsesler
+            // 
+            this.lstBitirilenProsesler.FormattingEnabled = true;
+            this.lstBitirilenProsesler.Location = new System.Drawing.Point(577, 85);
+            this.lstBitirilenProsesler.Name = "lstBitirilenProsesler";
+            this.lstBitirilenProsesler.Size = new System.Drawing.Size(239, 316);
+            this.lstBitirilenProsesler.TabIndex = 5;
+            // 
+            // btnBirilenProsesleriGoster
+            // 
+            this.btnBirilenProsesleriGoster.Location = new System.Drawing.Point(577, 64);
+            this.btnBirilenProsesleriGoster.Name = "btnBirilenProsesleriGoster";
+            this.btnBirilenProsesleriGoster.Size = new System.Drawing.Size(239, 23);
+            this.btnBirilenProsesleriGoster.TabIndex = 6;
+            this.btnBirilenProsesleriGoster.Text = "Bitirilen Prosesleri Göster";
+            this.btnBirilenProsesleriGoster.UseVisualStyleBackColor = true;
+            this.btnBirilenProsesleriGoster.Click += new System.EventHandler(this.btnBirilenProsesleriGoster_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Hız Ayarları (1 saniyede yapılacak işlem sayısı)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 436);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "İşlemci";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(230, 436);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Proses 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(401, 436);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Proses 2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(572, 436);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Proses 3";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(71, 476);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 9;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(245, 476);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 9;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(410, 476);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 9;
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(577, 476);
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(104, 45);
+            this.trackBar4.TabIndex = 9;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(26, 124);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(90, 186);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(122, 124);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(90, 186);
+            this.listBox2.TabIndex = 10;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(218, 124);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(90, 186);
+            this.listBox3.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 326);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Proses 1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(119, 326);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Proses 2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(215, 326);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Proses 3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "denemebutonu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(349, 124);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(83, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "İşlemci durumu: ";
+            // 
+            // lblIslemcıDurumu
+            // 
+            this.lblIslemcıDurumu.AutoSize = true;
+            this.lblIslemcıDurumu.Location = new System.Drawing.Point(439, 124);
+            this.lblIslemcıDurumu.Name = "lblIslemcıDurumu";
+            this.lblIslemcıDurumu.Size = new System.Drawing.Size(53, 13);
+            this.lblIslemcıDurumu.TabIndex = 13;
+            this.lblIslemcıDurumu.Text = "Çalışmıyor";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 24);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Kuyruğu temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 579);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblIslemcıDurumu);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBirilenProsesleriGoster);
+            this.Controls.Add(this.lstBitirilenProsesler);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnIslemciDurdur);
+            this.Controls.Add(this.btnIslemciBaslat);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "9888888888888";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnIslemciBaslat;
+        private System.Windows.Forms.Button btnIslemciDurdur;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ListBox lstBitirilenProsesler;
+        private System.Windows.Forms.Button btnBirilenProsesleriGoster;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblIslemcıDurumu;
+        private System.Windows.Forms.Button button2;
+        private System.Diagnostics.Process process1;
+    }
+}
+
---
+++ ProsesKuyruk/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProsesKuyruk
+{
+    public partial class Form1 : Form
+    {
+        /*Dictionary<string, Queue<string>> prosesler = new Dictionary<string, Queue<string>>()
+        {
+            { "Islemci", new Queue<string>() },
+            { "Proses1", new Queue<string>() },
+            { "Proses2", new Queue<string>() },
+            { "Proses3", new Queue<string>() },
+        };*/
+        bool islemciCalisiyor = false;
+        Queue<string> proses1 = new Queue<string>();
+        Queue<string> proses2 = new Queue<string>();
+        Queue<string> proses3 = new Queue<string>();
+        Stack<string> proses1Bitenler = new Stack<string>();
+        Stack<string> proses2Bitenler = new Stack<string>();
+        Stack<string> proses3Bitenler = new Stack<string>();
+        Dictionary<int, int> prosesHizlari = new Dictionary<int, int>() { };
+        //Stack<string> prosesler = new Stack<string>();
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            IslemciCalistir();
+        }
+        private void KuyrukSirala (ref Queue<string> kuyruk)
+        {
+            kuyruk = new Queue<string>(kuyruk.OrderBy(z => z));
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                proses1.Enqueue("P1-" + random.Next(0, 6));
+                proses2.Enqueue("P2-" + random.Next(0, 6));
+                proses3.Enqueue("P3-" + random.Next(0, 6));
+            }
+            ;
+            //proses1 = new Queue<string>(proses1.OrderBy(z => z));
+            KuyrukSirala(ref proses1);
+            KuyrukSirala(ref proses2);
+            KuyrukSirala(ref proses3);
+            listBox1.Items.AddRange(proses1.ToArray());
+            listBox2.Items.AddRange(proses2.ToArray());
+            listBox3.Items.AddRange(proses3.ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(trackBar1.Value.ToString());
+            //MessageBox.Show(proses1.Reverse().ToArray()[0]);
+            lstBitirilenProsesler.Items.Clear();
+            proses1Bitenler.Clear();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void btnBirilenProsesleriGoster_Click(object sender, EventArgs e)
+        {
+            lstBitirilenProsesler.Items.Clear();
+            Stack<string> gosterilecekKuyruk = new Stack<string>();
+            if (checkBox1.Checked)
+            {
+                Stack<string> tempQueue = proses1Bitenler;
+                foreach (var item in tempQueue)
+                {
+                    gosterilecekKuyruk.Push(item);
+                }
+            }
+            if (checkBox2.Checked)
+            {
+                Stack<string> tempQueue = proses2Bitenler;
+                foreach (var item in tempQueue)
+                {
+                    gosterilecekKuyruk.Push(item);
+                }
+            }
+            if (checkBox3.Checked)
+            {
+                Stack<string> tempQueue = proses3Bitenler;
+                foreach (var item in tempQueue)
+                {
+                    gosterilecekKuyruk.Push(item);
+                }
+            }
+
+            gosterilecekKuyruk = new Stack<string>(gosterilecekKuyruk.OrderBy(z => z));
+            foreach (var item in gosterilecekKuyruk)
+            {
+                lstBitirilenProsesler.Items.Add(item);
+            }
+            gosterilecekKuyruk = new Stack<string>();
+        }
+        private string[] ArrayBolme(string[] array, int baslangic = 0, int son = 0)
+        {
+            string[] cikti = new string[] {};
+            for (int i = baslangic; i < array.Length && i < son; i++)
+            {
+                cikti = cikti.Concat(new string[] { array[i] }).ToArray();
+            }
+            return cikti;
+        }
+        private async void IslemciCalistir()
+        {
+            while (true)
+            {
+                while (islemciCalisiyor)
+                {
+                    lblIslemcıDurumu.Text = "Çalışıyor";
+                    for (int i = 0; i < trackBar2.Value && i < proses1.Count ; i++)
+                    {
+                        string pt = proses1.Reverse().ToArray()[i];
+                        proses1Bitenler.Push(pt);
+                        proses1 = new Queue<string>(ArrayBolme(proses1.ToArray(), 1, proses1.Count));
+                        MessageBox.Show(proses1.Count.ToString());
+                        //proses1 = proses1.Reverse().ToArray();
+                    }
+                    for (int i = 0; i < trackBar3.Value && i < proses2.Count; i++)
+                    {
+                        string pt = proses2.Reverse().ToArray()[i];
+                        proses2Bitenler.Push(pt);
+                    }
+                    for (int i = 0; i < trackBar4.Value && i < proses3.Count; i++)
+                    {
+                        string pt = proses3.Reverse().ToArray()[i];
+                        proses3Bitenler.Push(pt);
+                    }
+                    await Task.Delay(999);
+                }
+                lblIslemcıDurumu.Text = "Çalışmıyor";
+                await Task.Delay(1);
+            }
+        }
+
+        private void btnIslemciBaslat_Click(object sender, EventArgs e)
+        {
+            islemciCalisiyor = true;
+        }
+
+        private void btnIslemciDurdur_Click(object sender, EventArgs e)
+        {
+            islemciCalisiyor = false;
+        }
+    }
+}
